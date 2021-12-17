802692ac747a53f98dfe411ea409e60b7f62082d
Rhys <72167211+Smunk3y@users.noreply.github.com>
2021-12-17 13:59:49 -0500

    Grade 10 Webby

---
+++ Grade10Website/login-page.js
@@ -0,0 +1,20 @@
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "user" && password === "cwdhs") {
+        location.reload();
+        window.location = "index.html";
+    } else {
+        loginErrorMsg.style.opacity = 1;
+        setTimeout(function(){
+        loginErrorMsg.style.opacity = 0;
+      }, 2000);
+    }
+})
