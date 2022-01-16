87e6b7f39d92153b5578d95914cf61934712855c
Rhys <72167211+Smunk3y@users.noreply.github.com>
2022-01-16 13:28:27 -0500

    adad

--- navbar.js
+++ navbar.js
@@ -253,7 +253,7 @@ class Header extends HTMLElement {
                               <span class="caret"></span>
                           </a>
                           <ul class="dropdown-menu backgroundblur">
-                              <li><a class="dropdowntext"  href="Vinyl.html">Vinyl Collection</a></li>
+                              <li><a class="dropdowntext"  href="vinyl.html">Vinyl Collection</a></li>
                               <li><a class="dropdowntext"  href="Scale.html">The Scale!</a></li>
                               <li><a class="dropdowntext"  href="AlbumIdeas.html">Suggest A Album</a></li>
                               <li><a class="dropdowntext"  href="Upcoming.html">Upcoming Albums</a></li>
